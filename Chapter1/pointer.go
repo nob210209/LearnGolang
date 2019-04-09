ea20acdddc8799f0ceb3ed61da8a9a0c9f77c5be
nob210209 <s1210209ryot@gmail.com>
2019-04-10 00:04:15 +0900

    ポインタ渡しと値渡しについて

--- Chapter1/pointer.go
+++ Chapter1/pointer.go
@@ -19,4 +19,19 @@ func main() {
 	fmt.Println("nの値：", n)
 	// *(間接参照演算子）を利用して、ポインタの中身を取得
 	fmt.Println("pointerの中身：", *pointer)
+
+	a, b := 10, 10
+
+	// aはそのまま、bはアドレス演算子をつけて呼び出す
+	called(a, &b)
+
+	fmt.Println("値渡し：", a)
+	fmt.Println("ポインタ渡し：", b)
+}
+
+func called(a int, b *int) {
+	// 変数をそのまま変更
+	a = a + 1
+	// 変数の中身を変更
+	*b = *b + 1
 }
