f305b1355c302054aac2ebe07f31e26d8e83bf76
nob210209 <s1210209ryot@gmail.com>
2019-04-11 00:24:09 +0900

    ラベルについて

---
+++ Chapter1/label.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+FOR_LABEL:
+	for i := 0; i < 10; i++ {
+		switch {
+		case i == 3:
+			// for文からの脱出
+			break FOR_LABEL
+
+		default:
+			fmt.Println(i)
+		}
+	}
+
+LABEL1:
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if i == 0 && j == 5 {
+				// 1番目のforへcontinue
+				continue LABEL1
+			} else if i == 1 && j == 1 {
+				// 2番目のforへcontinue
+				continue
+			}
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		if i == 2 {
+			// for文の外にあるLABELへ移動
+			goto LABEL
+		}
+		//fmt.Println(i)
+	}
+LABEL:
+}
